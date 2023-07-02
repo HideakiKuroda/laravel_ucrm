7335d95455f7f182248806a8f3e8a5ed02722db2
HideakiKuroda <ntb.kurodah@gmail.com>
2023-07-02 10:22:09 +0900

    CustomerSerch

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -1,14 +1,15 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { reactive } from 'vue'
+import { onMounted,reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
 import { Core as YubinBangoCore } from "yubinbango-core2";
+import * as AutoKana from "vanilla-autokana";
 
 const form = reactive({     //入力したデータをactiveに変数に
     name: null, 
-    kana: null, 
+    kana: null,
     tel: null, 
     email: null, 
     postcode: null,
@@ -16,7 +17,19 @@ const form = reactive({     //入力したデータをactiveに変数に
     birthday: null, 
     gender: null, 
     memo: null
-}) 
+}); 
+
+//名前の入力でフリカナが入りようにした。
+let autokana;
+
+onMounted(() => {     //何故か分らんけどこれが必要
+    autokana = AutoKana.bind('#name', '#kana',{katakana: true});
+});
+
+const handleNameInput = () => {
+    form.kana = autokana.getFurigana();
+}
+//ここまで
 
 const fetchAddress = () => {
     new YubinBangoCore(String(form.postcode), (value)=>{
@@ -58,14 +71,14 @@ defineProps({
                                 <div class="p-2 w-full">
                                     <div class="relative">
                                     <label for="name" class="leading-7 text-sm text-gray-600">顧客氏名</label>
-                                    <input type="text" id="name" name="name" v-model="form.name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="text" @input="handleNameInput" id="name" name="name" v-model="form.name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     </div>
                                 </div>
 
                                 <div class="p-2 w-full">
                                     <div class="relative">
                                     <label for="kana" class="leading-7 text-sm text-gray-600">顧客カナ</label>
-                                    <input type="text" id="kana" name="kana" v-model="form.kana" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="text"  id="kana" name="kana" v-model="form.kana" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     </div>
                                 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
 use App\Models\Item;
-
+use App\Models\Purchase;
 
 Route::resource('items', ItemController::class)
     ->middleware(['auth', 'verified']);
@@ -16,6 +17,8 @@ Route::resource('items', ItemController::class)
 Route::resource('customers', CustomerController::class)
 ->middleware(['auth', 'verified']);    
 
+Route::resource('purchases', PurchaseController::class)
+    ->middleware(['auth', 'verified']);
 
 Route::inertia('/beginer/app','Beginer/App');
 Route::inertia('/beginer/appemit','Beginer/AppEmit');
